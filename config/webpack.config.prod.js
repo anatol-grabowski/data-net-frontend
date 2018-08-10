a232940eb733c0157a0efea4b13fee6614bec116
tota <grabantot@gmail.com>
2018-08-10 08:31:41 +0300

    added suggested rule to webpack.config.prod.js - not much help

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -153,6 +153,13 @@ module.exports = {
               compact: true,
             },
           },
+          {
+            test: require.resolve('data-net'),
+            loader: require.resolve('babel-loader'),
+            options: {
+              compact: true,
+            },
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
